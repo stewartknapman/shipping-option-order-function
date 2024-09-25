7e4313010583dc8afe6434901890d51b7d20e0ab
Stewart Knapman <info@stewartknapman.com>
2024-09-26 11:39:43 +1200

    merge all carts

--- extensions/demo-cart-transform-extension/src/run.js
+++ extensions/demo-cart-transform-extension/src/run.js
@@ -17,5 +17,23 @@ const NO_CHANGES = {
  * @returns {FunctionRunResult}
  */
 export function run(input) {
-  return NO_CHANGES;
+  // return NO_CHANGES;
+
+  return {
+    "operations": [
+      {
+        "merge": {
+          "cartLines": input.cart.lines,
+          "parentVariantId": "gid://shopify/ProductVariant/10102625894673",
+          "price": {
+            // "percentageDecrease": {
+            //   "value": 10.5
+            // }
+          },
+          "title": "Snowboard Kit"
+        }
+      }
+    ]
+  }
+  
 };
